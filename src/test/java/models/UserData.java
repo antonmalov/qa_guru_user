6fe76490122c798967b488ebbefdc615f2a8d318
Anton Malov <maloanto@yandex.ru>
2023-07-01 01:15:37 +0300

    added lombok for user

--- src/test/java/models/UserData.java
+++ src/test/java/models/UserData.java
@@ -1,16 +1,10 @@
 package models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
 
+@Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class UserData {
     private User data;
-
-    public User getData() {
-        return data;
-    }
-
-    public void setData(User data) {
-        this.data = data;
-    }
 }
